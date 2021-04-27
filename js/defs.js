94834d9c1d819ece430aade24a2973b76a157e06
travka47 <travka.eal@students.dvfu.ru>
2021-04-27 23:17:18 +1000

    update1

---
+++ js/defs.js
@@ -0,0 +1,28 @@
+const ImageFiles = [
+    'playerShip3_orange'
+];
+
+const GameSettings = {
+    keyPress: {
+        left: 37,
+        right: 39,
+        up: 38,
+        down: 40,
+        space: 32
+    },
+    playAreaWidth: 720,
+    playAreaHeight: 576,
+    playAreaDiv: '#playArea',
+
+    playerDivName: 'playerSprite',
+    playerStart: {
+        x: 360,
+        y: 440
+    }
+};
+
+
+let GameManager = {
+    assets: {},
+    player: undefined
+};
